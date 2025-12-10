21e7370775f529ae0968e26f0b9abc272d1c14d3
Usman Abdullah <usmanabdullahbn@gmail.com>
2025-12-10 19:24:22 +0500

    ...

--- config/constants.js
+++ config/constants.js
@@ -1,4 +1,4 @@
-export const ROLES = ["admin", "manager", "supervisor", "technician", "Employee"];
+export const ROLES = ["admin", "manager", "supervisor", "technician", "employee"];
 
 export const PROJECT_STATUS = ["Active", "Completed", "On Hold", "Cancelled"];
 export const ORDER_STATUS = [
