8739999ae3f65b22b666c7e40726898109db1c8d
Usman Abdullah <usmanabdullahbn@gmail.com>
2025-12-17 20:49:35 +0500

    ...

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,28 +1,37 @@
-import Asset from "../models/Asset.js"
-import Customer from "../models/Customer.js"
-import Order from "../models/Order.js"
-import Project from "../models/Project.js"
-import Task from "../models/Task.js"
-import User from "../models/User.js"
+import Asset from "../models/Asset.js";
+import Customer from "../models/Customer.js";
+import Order from "../models/Order.js";
+import Project from "../models/Project.js";
+import Task from "../models/Task.js";
+import User from "../models/User.js";
 
 export const getDashboardStats = async (req, res, next) => {
   try {
-    const totalEmployees = await User.countDocuments()
-    const totalCustomers = await Customer.countDocuments()
-    const totalProjects = await Project.countDocuments()
-    const totalOrders = await Order.countDocuments()
-    const totalTasks = await Task.countDocuments()
-    const totalAssets = await Asset.countDocuments()
+    const totalEmployees = await User.countDocuments();
+    const totalCustomers = await Customer.countDocuments();
+    const totalProjects = await Project.countDocuments();
+    const totalOrders = await Order.countDocuments();
+    const totalTasks = await Task.countDocuments();
+    const totalAssets = await Asset.countDocuments();
     // const totalEmployees = await Employee.countDocuments()
 
-    const activeProjects = await Project.countDocuments({ status: "Active" })
-    const completedProjects = await Project.countDocuments({ status: "Completed" })
-    const completedTasks = await Task.countDocuments({ status: "Completed" })
-    const inProgressTasks = await Task.countDocuments({ status: "In Progress" })
-    const pendingOrders = await Order.countDocuments({ status: "Pending" })
-    const completedOrders = await Order.countDocuments({ status: "Completed" })
-    const inProgressOrders = await Order.countDocuments({ status: "In Progress" })
-    const cancelledOrders = await Order.countDocuments({ status: "Cancelled" })
+    const activeProjects = await Project.countDocuments({ status: "Active" });
+    const completedProjects = await Project.countDocuments({
+      status: "Completed",
+    });
+    const completedTasks = await Task.countDocuments({ status: "Completed" });
+    const inProgressTasks = await Task.countDocuments({
+      status: "In Progress",
+    });
+    const TodoTasks = await Task.countDocuments({ status: "Todo" });
+    const OnHoldTasks = await Task.countDocuments({ status: "On Hold" });
+
+    const pendingOrders = await Order.countDocuments({ status: "Pending" });
+    const completedOrders = await Order.countDocuments({ status: "Completed" });
+    const inProgressOrders = await Order.countDocuments({
+      status: "In Progress",
+    });
+    const cancelledOrders = await Order.countDocuments({ status: "Cancelled" });
 
     res.status(200).json({
       success: true,
@@ -41,20 +50,28 @@ export const getDashboardStats = async (req, res, next) => {
         completedOrders,
         inProgressOrders,
         cancelledOrders,
+        TodoTasks,
+        OnHoldTasks,
       },
-    })
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const getRecentActivities = async (req, res, next) => {
   try {
-    const recentOrders = await Order.find().populate("customer_id").limit(5).sort({ created_at: -1 })
+    const recentOrders = await Order.find()
+      .populate("customer_id")
+      .limit(5)
+      .sort({ created_at: -1 });
 
-    const recentProjects = await Project.find().populate("customer_id").limit(5).sort({ created_at: -1 })
+    const recentProjects = await Project.find()
+      .populate("customer_id")
+      .limit(5)
+      .sort({ created_at: -1 });
 
-    const recentTasks = await Task.find().limit(5).sort({ created_at: -1 })
+    const recentTasks = await Task.find().limit(5).sort({ created_at: -1 });
 
     res.status(200).json({
       success: true,
@@ -63,8 +80,8 @@ export const getRecentActivities = async (req, res, next) => {
         recentProjects,
         recentTasks,
       },
-    })
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
